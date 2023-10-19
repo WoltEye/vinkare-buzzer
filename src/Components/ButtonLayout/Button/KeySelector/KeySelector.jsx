3394b65252f408df443699419de07fdf9e8c2639
WoltEye <eetumaenpaa2002@gmail.com>
2023-10-19 22:03:03 +0300

    Add countdown to keybinding overlay

--- src/Components/ButtonLayout/Button/KeySelector/KeySelector.jsx
+++ src/Components/ButtonLayout/Button/KeySelector/KeySelector.jsx
@@ -3,30 +3,22 @@ import SelectionOverlay from './SelectionOverlay/SelectionOverlay';
 
 export default function KeySelector({ setKey, kbKey }) {
   const [ selectionOverlay, setSelectionOverlay ] = useState(false);
-  
+
   let timeoutId;
+  let intervalId;
 
   const handleClick = (e) => {
     e.target.blur();
     setSelectionOverlay(true);
-    timeoutId = setTimeout(() => {
-      setSelectionOverlay(false);
-    }, 5000)
-  }
-
-  useEffect(() => {
-    return () => {
-      setSelectionOverlay(false);
-      clearTimeout(timeoutId);
-    }
-  }, [])
+  } 
 
   return (
     <>
     { selectionOverlay &&
       <SelectionOverlay 
       overlay={setSelectionOverlay}
-      changeKey={setKey} />
+      changeKey={setKey}
+      />
     }
     <button onClick={handleClick}>
     { kbKey === ' ' ?
--- src/Components/ButtonLayout/Button/KeySelector/SelectionOverlay/SelectionOverlay.jsx
+++ src/Components/ButtonLayout/Button/KeySelector/SelectionOverlay/SelectionOverlay.jsx
@@ -1,23 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './SelectionOverlay.css';
 
 export default function SelectionOverlay({ overlay, changeKey }) {
+  const [ intervalId, setIntervalId ] = useState(null);
+  const [ closeOverlayTimer, setCloseOverlayTimer ] = useState(5); //In seconds
+  
   const onKeyDown = e => {
-    changeKey(e.key);
-    overlay(false);
+    if(e.key === 'Escape') {
+      overlay(false);
+    } else {
+      changeKey(e.key);
+      overlay(false);
+    }
   }
 
   useEffect(() => {
   document.addEventListener('keydown', onKeyDown);
+  const interval = setInterval(() => {
+    if(closeOverlayTimer > 0) {
+      setCloseOverlayTimer(prev => prev - 1);
+    }
+  }, 1000);
+  setIntervalId(interval);
   return () => {
     document.removeEventListener('keydown', onKeyDown);
+    clearInterval(interval);
+    setCloseOverlayTimer(5);
   }
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if(closeOverlayTimer <= 0) {
+      clearInterval(intervalId);
+      setCloseOverlayTimer(5);
+      overlay(false);
+    }
+  }, [closeOverlayTimer])
 
   return (
     <div 
     className='selection-overlay'>
-      <h2>Paina näppäintä jonka haluat asettaa</h2>
+      <h2>
+        Paina näppäintä jonka haluat asettaa
+      </h2>
+      <p>{closeOverlayTimer}</p>
+      <p className='tip'>Paina esc päästäksesi takaisin asetuksiin vaihtamatta näppäintä</p>
     </div>
   )
 }
